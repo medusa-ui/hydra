868b8a388c6e7cb547fd46f6f94b1bc66b020d05
Kevin Deyne <kevindeyne@gmail.com>
2021-10-05 23:18:42 -0700

    Menu item registry should be able to remove items as they become unavailable

--- hydra-core/src/main/java/io/getmedusa/hydra/discovery/registry/InMemoryRegistry.java
+++ hydra-core/src/main/java/io/getmedusa/hydra/discovery/registry/InMemoryRegistry.java
@@ -4,16 +4,14 @@ import io.getmedusa.hydra.discovery.model.ActiveService;
 import io.getmedusa.hydra.discovery.model.MenuItem;
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @Component
 public class InMemoryRegistry {
 
     private final Map<String, ActiveService> sessionMap = new HashMap<>();
     private final Map<String, List<MenuItem>> menuItems = new HashMap<>();
+    private final Map<String, Set<String>> menuSessions = new HashMap<>();
 
     public Map<String, ActiveService> getSessionMap() {
         return sessionMap;
@@ -25,25 +23,44 @@ public class InMemoryRegistry {
 
         for(Map.Entry<String, List<MenuItem>> menuItem : activeService.getMenuItems().entrySet()) {
             final String key = menuItem.getKey();
-            final List<MenuItem> items = menuItem.getValue();
+            final List<MenuItem> items = new ArrayList<>(menuItem.getValue());
             if(this.menuItems.containsKey(key)) {
                 this.menuItems.get(key).addAll(items);
             } else {
                 this.menuItems.put(key, items);
             }
         }
+        this.menuSessions.put(sessionId, activeService.getMenuItems().keySet());
     }
 
     public ActiveService getAndRemove(String sessionId) {
         final ActiveService activeService = this.sessionMap.get(sessionId);
         this.sessionMap.remove(sessionId);
+
+        Set<String> keysPotentiallyDeleted = new HashSet<>(this.menuSessions.get(sessionId));
+        this.menuSessions.remove(sessionId);
+        for(String keyToPotentiallyDeleted : keysPotentiallyDeleted) {
+            if(!isPresentWithOtherSession(keyToPotentiallyDeleted)) {
+                this.menuItems.remove(keyToPotentiallyDeleted);
+            }
+        }
+
         return activeService;
+    }
+
+    private boolean isPresentWithOtherSession(String key) {
+        for(Set<String> sessionKeys : this.menuSessions.values()) {
+            if(sessionKeys.contains(key)){
+                return true;
+            }
+        }
+        return false;
     }
 
     public KnownRoutes toURLMap() {
         KnownRoutes knownRoutes = new KnownRoutes();
         List<KnownRoute> routeList = new ArrayList<>();
-        for(Map.Entry<String, ActiveService> sessionEntry : sessionMap.entrySet()) {
+        for(Map.Entry<String, ActiveService> sessionEntry : sessionMap.entrySet()) { //TODO possible issue with concurrent map
             KnownRoute route = new KnownRoute();
             route.setService(sessionEntry.getValue().getName());
             route.setAvailableRoutes(sessionEntry.getValue().getEndpoints());
--- hydra-core/src/test/java/io/getmedusa/hydra/InMemoryRegistryTest.java
+++ hydra-core/src/test/java/io/getmedusa/hydra/InMemoryRegistryTest.java
@@ -1,6 +1,7 @@
 package io.getmedusa.hydra;
 
 import io.getmedusa.hydra.discovery.model.ActiveService;
+import io.getmedusa.hydra.discovery.model.MenuItem;
 import io.getmedusa.hydra.discovery.registry.InMemoryRegistry;
 import io.getmedusa.hydra.discovery.registry.KnownRoute;
 import io.getmedusa.hydra.discovery.registry.KnownRoutes;
@@ -36,6 +37,56 @@ class InMemoryRegistryTest {
             final String service = key.getService();
             Assertions.assertTrue("serviceA".equals(service) || "serviceB".equals(service));
         }
+    }
+
+    @Test
+    void testMenusWithMultipleSessions() {
+        final MenuItem item = new MenuItem();
+        item.setEndpoint("/123");
+        item.setLabel("My label");
+        Map<String, List<MenuItem>> menuItemsA = Map.of("top-menu", Collections.singletonList(item));
+
+        final MenuItem item2 = new MenuItem();
+        item2.setEndpoint("/543");
+        item.setLabel("Other label");
+        Map<String, List<MenuItem>> menuItemsB = Map.of("bottom-menu", Collections.singletonList(item2));
+
+        ActiveService activeServiceA1 = new ActiveService();
+        activeServiceA1.setHost("127.0.0.111");
+        activeServiceA1.setPort(8080);
+        activeServiceA1.setName("serviceA");
+        activeServiceA1.setEndpoints(new HashSet<>(Arrays.asList("/page3", "/page4")));
+        activeServiceA1.setMenuItems(menuItemsA);
+
+        ActiveService activeServiceA2 = new ActiveService();
+        activeServiceA2.setHost("127.0.0.112");
+        activeServiceA2.setPort(8080);
+        activeServiceA2.setName("serviceA");
+        activeServiceA2.setEndpoints(new HashSet<>(Arrays.asList("/page3", "/page4")));
+        activeServiceA2.setMenuItems(menuItemsA);
+
+        ActiveService activeServiceB = new ActiveService();
+        activeServiceB.setHost("127.0.0.50");
+        activeServiceB.setPort(8081);
+        activeServiceB.setName("serviceB");
+        activeServiceB.setEndpoints(new HashSet<>(List.of("/page")));
+        activeServiceB.setMenuItems(menuItemsB);
+
+        String sessionA1 = UUID.randomUUID().toString();
+        String sessionA2 = UUID.randomUUID().toString();
+        String sessionB = UUID.randomUUID().toString();
+
+        registry.add(sessionA1, activeServiceA1);
+        registry.add(sessionA2, activeServiceA2);
+        registry.add(sessionB, activeServiceB);
+
+        Assertions.assertEquals(2, registry.getMenuItems().size());
+        registry.getAndRemove(sessionA2);
+        Assertions.assertEquals(2, registry.getMenuItems().size());
+        registry.getAndRemove(sessionA1);
+        Assertions.assertEquals(1, registry.getMenuItems().size());
+        registry.getAndRemove(sessionB);
+        Assertions.assertEquals(0, registry.getMenuItems().size());
     }
 
 }
