5aec5bd8adbd74aea8036708ff7e2847916658ad
Kevin Deyne <kevindeyne@gmail.com>
2023-01-12 21:05:55 -0800

    Custom login page for Hydra

--- hydra/src/main/java/io/getmedusa/hydra/core/config/SecurityConfig.java
+++ hydra/src/main/java/io/getmedusa/hydra/core/config/SecurityConfig.java
@@ -5,12 +5,19 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.MediaType;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
 
 import static org.springframework.security.web.server.header.ReferrerPolicyServerHttpHeadersWriter.ReferrerPolicy.SAME_ORIGIN;
 import static org.springframework.security.web.server.header.XFrameOptionsServerHttpHeadersWriter.Mode.SAMEORIGIN;
+import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
 
 @EnableWebFluxSecurity
 @Configuration
@@ -31,10 +38,20 @@ public class SecurityConfig {
                         .frameOptions(frameOptions -> frameOptions.mode(SAMEORIGIN))
                         .referrerPolicy(SAME_ORIGIN))
                 .authorizeExchange()
-                .anyExchange().permitAll()
+                .pathMatchers("/login").permitAll()
+                .anyExchange().authenticated()
                 .and()
-                .formLogin(form -> form.authenticationSuccessHandler(new HydraAuthSuccessHandler(jwtTokenService))) //TODO make redirect either a property or dynamic based on referer
+                .formLogin(form -> form.authenticationSuccessHandler(new HydraAuthSuccessHandler(jwtTokenService)).loginPage("/login"))
                 .csrf().disable() //TODO re-enable this when using gRPC instead of REST for medusa calls
                 .build();
+    }
+
+    @Bean
+    RouterFunction<ServerResponse> routes() {
+        return route(GET("/login"), this::handleLogin);
+    }
+
+    private Mono<ServerResponse> handleLogin(ServerRequest req) {
+        return ServerResponse.ok().contentType(MediaType.TEXT_HTML).render("login");
     }
 }
